b3c3a43727476f837a26198a1665207aa5f4e3a3
ryan-kim-dev <ryankim.h.dev@gmail.com>
2023-04-05 14:59:15 +0900

    Refactor: Allow cors on preflight request

--- index.js
+++ index.js
@@ -13,11 +13,7 @@ const openai = new OpenAIApi(config);
 
 // OpenAI & TMDB 요청의 프록시를 위한 express 서버 구현
 const app = express();
-app.use(
-  cors({
-    origin: ['http://localhost:3000', 'https://what-movie.vercel.app'],
-  })
-);
+app.options('*', cors());
 app.use(bodyParser.json());
 
 // * 1. OpenAI에게 영화 추천 요청 응답을 위한 라우터
