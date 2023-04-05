10b364e988fcbfce27fc69f1b2fcaf64770561e2
ryan-kim-dev <ryankim.h.dev@gmail.com>
2023-04-05 14:15:07 +0900

    Feat: Use cors middleware

--- index.js
+++ index.js
@@ -13,18 +13,19 @@ const openai = new OpenAIApi(config);
 
 // OpenAI & TMDB 요청의 프록시를 위한 express 서버 구현
 const app = express();
+app.use(cors());
 app.use(bodyParser.json());
 app.use(cors());
 
 // * 1. OpenAI에게 영화 추천 요청 응답을 위한 라우터
 app.post('/recommendations', async (req, res) => {
-  const { query } = req.body;
+  const { prompt } = req.body;
 
   const completion = await openai.createCompletion({
     model: 'text-davinci-003',
     max_tokens: 50,
     temperature: 0.5, // 결과의 다양성 수준 조절 (0 ~ 2)
-    prompt: prompt,
+    prompt: `suggest four moives most related to ${prompt}`,
   });
 
   res.send(completion.data.choices[0].text);
